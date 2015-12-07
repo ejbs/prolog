cb977c15e67fad76ccce1e6d437aa6c917fa301c
ejbs <emiljbs@gmail.com>
2015-12-07 02:12:53 +0100

    Fuck this, reverting to last known good state

--- labb2.pl
+++ labb2.pl
@@ -9,9 +9,8 @@ properties(L,S,R):-
 unvisited(T,S,U,R):-
     children(T,S,R1),
     remove_duplicates(U,R1,R).
-already_visited(S,U):-
-    memberchk(S,U).
 
+%Implementera remove_duplicates
 remove_duplicates(L1,L2,R):-
     append(L1,L2,L3),
     rem_dups(L3,[],R).
@@ -26,38 +25,39 @@ some( C_2, [X|Xs]) :-
    ( some(C_2, Xs) ).
 
 every(_C_2, []).
-every( C_2, [X|Xs]) :-
+every(C_2, [X|Xs]) :-
     call(C_2, X),
     every(C_2, Xs).
 
 satisfies(T,L,U,ax(F),S):-
     unvisited(T,S,U,R),
-    every(satisfies(T,L,U,F),R).
+    every(satisfies(T,L,[U|S],F),R).
 satisfies(T,L,U,ex(F),S):-
     unvisited(T,S,U,R),
-    some(satisfies(T,L,U,F),R).
-
+    some(satisfies(T,L,[U|S],F),R).
 satisfies(T,L,U,eg(F),S):-
     unvisited(T,S,U,R),
-    ((\+ memberchk(S,U)), satisfies(T,L,[S|U],F,S)),
-    some(satisfies(T,L,[S|U],eg(F)),R).
+    satisfies(T,L,U,F,S),
+    some(satisfies(T,L,[U|S],eg(F)),R).
 satisfies(T,L,U,ag(F),S):-
     unvisited(T,S,U,R),
-    ((\+ memberchk(S,U)), satisfies(T,L,[S|U],F,S)),
+    satisfies(T,L,U,F,S),
     every(satisfies(T,L,[U|S],ag(F)),R).
-
 satisfies(T,L,U,af(F),S):-
     unvisited(T,S,U,R),
-    every(recur_af(T,L,[S|U],F),R).
+    every(recur_af(T,L,[U|S],F),R).
 satisfies(T,L,U,ef(F),S):-
     unvisited(T,S,U,R),
-    some(recur_ef(T,L,[S|U],F),R).
+    some(recur_ef(T,L,[U|S],F),R).
+satisfies(T,L,U,af(F),S):-
+    unvisited(T,S,U,R),
+    some(recur_af(T,L,[U|S],F),R).
 
-satisfies(T,L,U,neg(F),S):-
+satisfies(T,L,U,not(F),S):-
     \+ satisfies(T,L,U,F,S).
 satisfies(T,L,U,or(X,Y),S):-
-    (satisfies(T,L,U,X,S);
-    satisfies(T,L,U,Y,S)).
+    satisfies(T,L,U,X,S);
+    satisfies(T,L,U,Y,S).
 satisfies(T,L,U,and(X,Y),S):-
     satisfies(T,L,U,X,S),
     satisfies(T,L,U,Y,S).
@@ -66,11 +66,11 @@ satisfies(T,L,U,Atom,S):-
     memberchk(Atom,R).
 
 recur_ef(T,L,U,F,S):-
-    ((\+ memberchk(S,U)),satisfies(T,L,U,F,S));
-    satisfies(T,L,[S|U],ef(F),S).
+    satisfies(T,L,U,F,S);
+    satisfies(T,L,U,ef(F),S).
 recur_af(T,L,U,F,S):-
-    ((\+ memberchk(S,U)),satisfies(T,L,U,F,S));
-    satisfies(T,L,[S|U],af(F),S)).
+    satisfies(T,L,U,F,S);
+    satisfies(T,L,U,af(F),S).
 
 verify(Input) :-
     see(Input), read(T), read(L), read(S), read(F), seen,
