06d79d7b195972150dcc0770f34a88b079cb26e8
ejbs <emiljbs@gmail.com>
2015-12-14 22:56:01 +0100

    durr

--- labb2.pl
+++ labb2.pl
@@ -41,7 +41,7 @@ accumulating_every(T,L,U,F,[S|Ss]):-
     satisfies(T,L,U,F,S),
     accumulating_every(T,L,[S|U],F,Ss).
 
-accumulating_some(_,_,_,_,[]).
+accumulating_some(_,_,_,_,[]):- is_list(a).
 accumulating_some(T,L,U,F,[S|Ss]):-
     satisfies(T,L,U,F,S);
     accumulating_some(T,L,[S|U],F,Ss).
