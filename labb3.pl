66f2d301d3f00bdb165a8482fab082f55dda9e6a
ejbs <emiljbs@gmail.com>
2015-12-15 16:02:31 +0100

    reimplement ef

--- labb3.pl
+++ labb3.pl
@@ -1,7 +1,5 @@
 % check(T,L,S,U,F)
 % satisfies(T,L,U,F,S)
-
-
 children(T,S,R):-
     memberchk([S,R],T).
 
@@ -13,6 +11,7 @@ every(T,L,U,F,[S|Ss]):-
     satisfies(T,L,U,F,S),
     every(T,L,U,F,Ss).
 
+some(_,_,_,_,[]).
 some(T,L,U,F,[S|Ss]):-
     satisfies(T,L,U,F,S);
     some(T,L,U,F,Ss).
@@ -42,13 +41,12 @@ satisfies(T,L,U,af(F),S):-
       every(T,L,[S|U],af(F),R)
      )
     ).
+
 satisfies(T,L,U,ef(F),S):-
     \+ memberchk(S,U),
-    (satisfies(T,L,S,[],F);
-     (children(T,S,R),
-      some(T,L,[S|U],ef(F),R)
-     )
-    ).
+    ef1(T,L,U,F,S);
+    ef2(T,L,U,F,S).
+
 satisfies(T,L,U,neg(Atom),S):-
     properties(L,S,R),
     \+ memberchk(Atom,R).
@@ -62,6 +60,10 @@ satisfies(T,L,U,Atom,S):-
     properties(L,S,R),
     memberchk(Atom,R).
 
+ef1(T,L,U,F,S):-
+    satisfies(T,L,[],F,S).
+ef2(T,L,U,F,S):-
+    some(T,L,[S|U],ef(F),S).
 	
 verify(Input) :-
     see(Input), read(T), read(L), read(S), read(F), seen,
